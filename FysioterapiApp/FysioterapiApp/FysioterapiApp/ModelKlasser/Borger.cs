6e3440b74a5fd5ed09d287f5a652d9d23541c8b4
Unknown <lundsgaard1715@hotmail.com>
2018-10-31 12:36:05 +0100

    Opsat model klasser på ny
    Borger model klasse er lavet og done.

---
+++ FysioterapiApp/FysioterapiApp/FysioterapiApp/ModelKlasser/Borger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FysioterapiApp.ModelKlasser
+{
+    public class Borger
+    {
+
+        public string FName { get; set; }
+
+        public string LName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Address { get; set; }
+
+        public int Ssn { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
